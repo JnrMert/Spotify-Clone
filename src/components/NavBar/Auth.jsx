3aecaedb54f66f563086eb164e2792a4e811997e
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-09-17 20:53:44 +0300

    Menu build

--- src/components/NavBar/Auth.jsx
+++ src/components/NavBar/Auth.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 function Auth() {
+  const { user } = {
+    name: "Mert Yazıcı",
+    avatar: "https://i.scdn.co/image/ab67757000003b82953b1990f385738c7ec2c8ef",
+  };
   return (
-    <nav>
-      <a href="">Auth</a>
-    </nav>
+    <Menu>
+      <MenuButton>
+        <img src={user.avatar} alt="" />
+        <span>{user.name}</span>
+      </MenuButton>
+      <MenuItems anchor="bottom">
+        <MenuItem>
+          <a className="block data-[focus]:bg-blue-100" href="/settings">
+            Settings
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a className="block data-[focus]:bg-blue-100" href="/support">
+            Support
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a className="block data-[focus]:bg-blue-100" href="/license">
+            License
+          </a>
+        </MenuItem>
+      </MenuItems>
+    </Menu>
   );
 }
 
